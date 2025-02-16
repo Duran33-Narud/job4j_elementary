82f26db7615c57a34834ad417965cf3f4f2e84c3
Duran33-Narud <nevermoresf228@gmail.com>
2025-02-16 15:01:58 +0300

    Сумма чисел [#505208]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        if (start > finish) {
+            return 0;
+        }
+        for (int i = start; i <= finish; i++ ) {
+            sum = sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Сумма от 0 до 10 = " + sum(0, 10));
+        System.out.println("Сумма от 3 до 8 = " + sum(3, 8));
+        System.out.println("Сумма от 1 до 1 = " + sum(1, 1));
+        System.out.println("Сумма от 0 до 5 = " + sum(0, 5));
+        System.out.println("Сумма от 15 до 3 = " + sum(15, 3));
+        System.out.println("Сумма от -10 до 10 = " + sum(-10, 10));
+    }
+}
