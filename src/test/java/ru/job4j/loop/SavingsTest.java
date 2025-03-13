22206375f47fe877f237bb755a3bf9fcad2973e9
Duran33-Narud <nevermoresf228@gmail.com>
2025-03-13 17:34:11 +0300

    Цикл do-while [#505216 #535636] -Поменял местами строки -Удалил неиспользуемый "import"

--- src/test/java/ru/job4j/loop/SavingsTest.java
+++ src/test/java/ru/job4j/loop/SavingsTest.java
@@ -3,7 +3,6 @@ package ru.job4j.loop;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SavingsTest {
     @Test
