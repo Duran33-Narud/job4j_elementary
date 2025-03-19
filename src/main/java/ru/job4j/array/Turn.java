68ce4b8ce84f0fa9296959260a3ea014566c5397
Duran33-Narud <nevermoresf228@gmail.com>
2025-03-19 18:35:51 +0300

    Перевернуть массив [#505231] - Убрал ошибку по стилю

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int arr = array.length; //
+        int arr = array.length;
         for (int i = 0; i < arr / 2; i++) {
             int temp = array[i];
             array[i] = array[arr - 1 - i];
